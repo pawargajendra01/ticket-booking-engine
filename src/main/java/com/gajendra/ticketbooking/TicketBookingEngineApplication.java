dadc7de21a1c4756618022237b5172e298b03e2e
Gajendra Pawar <gajendra101004@gmail.com>
2026-08-10 18:11:04 +0530

    Initialize Spring Boot project scaffold with H2 and Maven wrapper

---
+++ src/main/java/com/gajendra/ticketbooking/TicketBookingEngineApplication.java
@@ -0,0 +1,13 @@
+package com.gajendra.ticketbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketBookingEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketBookingEngineApplication.class, args);
+	}
+
+}
